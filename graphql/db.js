2f659b043f06b3d8f407b77cb2b46da651440da9
이기림(Kirim Lee)/Client개발팀/SKTECH <kirim@sk.com>
2018-10-07 13:02:21 +0900

    API 호출로 변경

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,9 @@
-let movies = [
-    {
-      id: 0,
-      name: "Star Wars - The new one",
-      score: 1
-    },
-    {
-      id: 1,
-      name: "Avengers - The new one",
-      score: 8
-    },
-    {
-      id: 2,
-      name: "The Godfather I",
-      score: 99
-    },
-    {
-      id: 3,
-      name: "Logan",
-      score: 2
-    }
-  ];
+import fetch from 'node-fetch';
+const API_URLS = "https://yts.ag/api/v2/list_movies.json";
 
-  export const getMovies = () => movies;
-
-export const getById = id => {
-  const filteredMovies = movies.filter(movie => movie.id === id);
-  return filteredMovies[0];
+export const getMovies = () => {
+  return fetch(`${API_URLS}`)
+    .then(res => res.json())
+    .then(json=>json.data.movies)
 };
 
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name,score)=>{
-  const newMovie={
-    name,
-    score,
-    id:`${movies.length+1}`
-  }
-  movies.push(newMovie);
-  return newMovie;
-}
