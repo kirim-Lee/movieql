11f1443f6d76579a5eb08c8a17136f1ead96dfac
이기림(Kirim Lee)/Client개발팀/SKTECH <kirim@sk.com>
2018-10-07 13:17:54 +0900

    API 호출 옵션추가

--- graphql/db.js
+++ graphql/db.js
@@ -1,8 +1,14 @@
 import fetch from 'node-fetch';
+import { isRegExp } from 'util';
 const API_URLS = "https://yts.ag/api/v2/list_movies.json";
 
-export const getMovies = () => {
-  return fetch(`${API_URLS}`)
+export const getMovies = (limit,rating) => {
+  let REQUEST_URLS = API_URLS;
+  const Option = [];
+  if(limit>0) Option.push(`limit=${limit}`);
+  if(rating>0) Option.push(`minimum_rating=${rating}`);
+  if(Option.length) REQUEST_URLS +=`?${Option.join('&')}`;
+  return fetch(REQUEST_URLS)
     .then(res => res.json())
     .then(json=>json.data.movies)
 };
